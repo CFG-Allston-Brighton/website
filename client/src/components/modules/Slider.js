c39fcd1314e2732e49d6e60074c62042f0f86d8d
Isha Agarwal <isha.agarwal3139@gmail.com>
2024-07-04 19:01:33 -0400

    set default year to 2000

--- client/src/components/modules/Slider.js
+++ client/src/components/modules/Slider.js
@@ -2,7 +2,7 @@ import React from "react";
 import Slider from "@mui/material/Slider";
 
 const YearSlider = (props) => {
-  const [value, setValue] = React.useState(1990);
+  const [value, setValue] = React.useState(2000);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
